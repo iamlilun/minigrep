ec41d3e25a98cd482a7e025e1fe5c248ec364303
lilun hsiao <iamlilun@gmail.com>
2021-09-02 12:22:58 +0800

    重構成struct..比較有關聯性跟可讀性

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,27 @@ use std::fs; //處理跟文件相關事務
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args); //拆分出函數..
+    let config = parse_config(&args); //拆分出函數..
 
-    println!("Search for {}", query);
-    println!("In file {}", filename);
+    println!("Search for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
     .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+//重構成struct比較有可讀性
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    //使用clone..花一些性能確可以省去維護借用生命周期的麻煩..是值得的
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config{query, filename}
 }
