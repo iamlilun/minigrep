1a337ec246530026d054cd110a5f9b310935d0db
lilun hsiao <iamlilun@gmail.com>
2021-09-02 12:26:39 +0800

    重構成impl..更有rust的味道

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs; //處理跟文件相關事務
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args); //拆分出函數..
+    let config = Config::new(&args); //拆分出函數..
 
     println!("Search for {}", config.query);
     println!("In file {}", config.filename);
@@ -20,10 +20,13 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    //使用clone..花一些性能確可以省去維護借用生命周期的麻煩..是值得的
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config{query, filename}
+impl Config {
+    //重構成impl..更有rust的味道
+    fn new(args: &[String]) -> Config {
+        //使用clone..放棄一些性能確可以省去維護借用生命周期的麻煩..是值得的
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Config{query, filename}
+    }
 }
