18487da4f221cc1ffa94e72cf35c87b2154d2c17
lilun hsiao <iamlilun@gmail.com>
2021-09-21 01:07:26 +0800

    Refactory 改用迭代方式替代clone..改用閉包+迭代方法替代for

--- src/lib.rs
+++ src/lib.rs
@@ -22,38 +22,38 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enugh arguments");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        //讀取變數..
-        //is_err如果有錯誤會回true..所以沒有設環境變數時..就會因為error回true->比對大小寫
+        
+        args.next(); //第一個參數不用
+        
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config{query, filename, case_sensitive})
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        } 
-    }
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    let query = query.to_ascii_lowercase();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        } 
-    }
-    results
+    contents.lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_ascii_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
