d564c19e6dac69351d99cbd7b6e1177e7fe84249
lilun hsiao <iamlilun@gmail.com>
2021-09-20 16:05:50 +0800

    Refactory main邏輯提取為函數

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 use std::env;
-use std::fs; //處理跟文件相關事務
-use std::process; //處理執行續
+use std::fs;
+use std::process;
+use std::error::Error; 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
-        process::exit(1) //離開程式
+        process::exit(1); //離開程式
     });
 
-    println!("Search for {}", config.query);
-    println!("In file {}", config.filename);
+    //因為成功時返回的是空元組..所以不用unwrap..直接用if let看有沒錯誤
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
 
-    let contents = fs::read_to_string(config.filename)
-    .expect("Something went wrong reading the file");
-
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
     println!("With text:\n{}", contents);
+    Ok(())
 }
 
 struct Config {
