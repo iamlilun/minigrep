f5eb92373c470d417705553b4d03b4654b758577
lilun hsiao <iamlilun@gmail.com>
2021-09-20 16:16:01 +0800

    Refactory 業務邏輯重構到lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error; 
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,32 +11,9 @@ fn main() {
     });
 
     //因為成功時返回的是空元組..所以不用unwrap..直接用if let看有沒錯誤
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    ///重構成用Result   
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enugh arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config{query, filename})
-    }
-}
